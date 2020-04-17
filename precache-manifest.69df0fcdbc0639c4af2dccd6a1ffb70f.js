39ee6dbd680f92f5e7405fec0107882c99ce58ed
Iryna <irynka1071994@gmail.com>
2020-04-17 04:09:25 +0300

    Updates

---
+++ precache-manifest.69df0fcdbc0639c4af2dccd6a1ffb70f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "871dc048373c5ea53a5f38068b37acf1",
+    "url": "/styledcomponent/index.html"
+  },
+  {
+    "revision": "2fcb5727162bda7d7e3c",
+    "url": "/styledcomponent/static/css/main.c682210a.chunk.css"
+  },
+  {
+    "revision": "5d88734610cd4e56d89e",
+    "url": "/styledcomponent/static/js/2.dcd542c9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/styledcomponent/static/js/2.dcd542c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fcb5727162bda7d7e3c",
+    "url": "/styledcomponent/static/js/main.6d87da8d.chunk.js"
+  },
+  {
+    "revision": "58521bb15fc48331e56c",
+    "url": "/styledcomponent/static/js/runtime-main.93c17aea.js"
+  }
+]);
